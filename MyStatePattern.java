b8d50c6a8bf3ae3bf15b1cc911e08719f089af3e
Arash Shadmani <shadmaniarash@gmail.com>
2018-03-16 13:34:39 -0400

    a minor correction in description

--- MyStatePattern.java
+++ MyStatePattern.java
@@ -17,7 +17,7 @@ public class MyStatePattern {
     public static void main(String[] args) {
         int distance;
         TextColor txtColor= null;
-        System.out.println("Showing Blue if distance is < 6 and Black otherwise and no change if color doesn't change:");
+        System.out.println("Showing Blue if distance is < 6, Black otherwise and no change if color doesn't change:");
         System.out.println("----------------------------------------------------------");
         for (int i=0; i<10; i++){
             distance = (int) (Math.random()*10);
